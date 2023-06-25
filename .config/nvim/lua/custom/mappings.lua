b5cfd4088fe71ef6c4d4aed635e3caef1817dfe4
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-06-25 16:25:25 +0200

    added vim tmux navigation

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,13 +1,25 @@
 local M = {}
 
+M.disabled = {
+  n = {
+    ["<C-h>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+    ["<C-l>"] = "",
+  }
+}
+
 M.abc = {
-  --n = {
-	--TODO
-  --}
+  n = {
+    -- ["<C-h>"] = { "<C-U>TmuxNavigateLeft<cr>", "Move cursor to left window (or tmux pane)" },
+    -- ["<C-j>"] = { "<C-U>TmuxNavigateDown<cr>", "Move cursor to bottom window (or tmux pane)" },
+    -- ["<C-k>"] = { "<C-U>TmuxNavigateUp<cr>", "Move cursor to top window (or tmux pane)" },
+    -- ["<C-l>"] = { "<C-U>TmuxNavigateRight<cr>", "Move cursor to right window (or tmux pane)" },
+  },
 
   i = {
      ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
-  }
+  },
 }
 
 return M
---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,5 @@
+local plugins = {
+  { 'christoomey/vim-tmux-navigator', lazy = false }
+}
+
+return plugins
