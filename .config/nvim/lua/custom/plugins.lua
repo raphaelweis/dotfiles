4b1e4d97af9527ff4e8cafc4d042fab4437615c8
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-07-07 21:45:40 +0200

    added null-ls basic configuration

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,24 +1,38 @@
 local plugins = {
-  { 'christoomey/vim-tmux-navigator', lazy = false },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = require 'custom.configs.treesitter',
-  },
-  {
-   "williamboman/mason.nvim",
-   opts = require 'custom.configs.mason',
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = require 'custom.configs.nvim-cmp'
-  }
+	{ "christoomey/vim-tmux-navigator", lazy = false },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = require("custom.configs.treesitter"),
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"williamboman/mason-null-ls.nvim",
+				config = function()
+					require("custom.configs.mason-null-ls")
+				end,
+			},
+			{
+				"williamboman/mason.nvim",
+				opts = require("custom.configs.mason"),
+			},
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		opts = require("custom.configs.nvim-cmp"),
+	},
 }
 
 return plugins
