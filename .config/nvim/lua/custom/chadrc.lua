b8b3c8c2e4c52a2bb42a3ec09556558d50510132
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-07-07 16:52:18 +0200

    status line configuration

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -7,7 +7,14 @@ M.ui = {
   theme = 'gruvbox',
   nvdash = {
     load_on_startup = true
-  }
+  },
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+
+    -- default/round/block/arrow (separators work only for "default" statusline theme;
+    -- round and block will work for the minimal theme only)
+    separator_style = "default",
+  },
 }
 return M
 
