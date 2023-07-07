280c2decd2e1babc703e2b8ad099eb3b927a1ce1
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-07-07 15:35:41 +0200

    added correct colortheme

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -3,6 +3,11 @@ local M = {}
 M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
 
-M.ui = {theme = 'onedark'}
+M.ui = {
+  theme = 'gruvbox',
+  -- nvdash = {
+  --   load_on_startup = true
+  -- }
+}
 return M
 
