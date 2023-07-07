56666afa49ff13b34ec320f7ca2fdee8895b953b
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-07-07 16:44:17 +0200

    added language servers configuration

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,5 +1,57 @@
 local plugins = {
-  { 'christoomey/vim-tmux-navigator', lazy = false }
+  { 'christoomey/vim-tmux-navigator', lazy = false },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "vue",
+        "svelte",
+        "dart",
+        "php",
+        "c",
+        "rust",
+        "zig"
+      },
+    },
+  },
+  {
+   "williamboman/mason.nvim",
+   opts = {
+      ensure_installed = {
+        "bash-language-server",
+        "clangd",
+        "cmake-language-server",
+        "css-lsp",
+        "dockerfile-language-server",
+        "html-lsp",
+        "json-lsp",
+        "jdtls",
+        "typescript-language-server",
+        "texlab",
+        "lua-language-server",
+        "marksman",
+        "intelephense",
+        "jedi-language-server",
+        "rust-analyzer",
+        "sqls",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
 }
 
 return plugins
