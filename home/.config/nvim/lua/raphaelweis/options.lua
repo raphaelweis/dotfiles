d1792b55e68a1778b1e6f27867e94c76cef38197
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-10-04 10:58:02 +0200

    Tweaked a few options

--- home/.config/nvim/lua/raphaelweis/options.lua
+++ home/.config/nvim/lua/raphaelweis/options.lua
@@ -3,52 +3,57 @@
 -----------------------------
 -- Set <space> as the leader key
 -- See `:help mapleader`
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.opt.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
 
 -- Configure tabstop and shiftwidth
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Enable relative line numbers
-vim.o.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.iskeyword:append("-")
 
 -- Enable break indent
-vim.o.breakindent = true
+vim.opt.breakindent = true
 
 -- Save undo history
-vim.o.undofile = true
+vim.opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.opt.completeopt = "menuone,noselect"
 
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
+
+-- backspace
+vim.opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- Providers
 vim.g.loaded_node_provider = 0
@@ -58,18 +63,18 @@ vim.g.loaded_ruby_provider = 0
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 	group = highlight_group,
-	pattern = '*',
+	pattern = "*",
 })
 
 -- [[ helper function for indentation based on filetype ]]
 local function setIndentation(filetype, shiftwidth, softtabstop, tabstop, expandtab)
-	vim.api.nvim_create_autocmd('FileType', {
+	vim.api.nvim_create_autocmd("FileType", {
 		callback = function()
 			vim.bo.shiftwidth = shiftwidth
 			vim.bo.softtabstop = softtabstop
@@ -81,7 +86,7 @@ local function setIndentation(filetype, shiftwidth, softtabstop, tabstop, expand
 end
 
 -- Set 2 spaces for these filetypes
-setIndentation('dart', 2, 2, 2, 1)
-setIndentation('c', 2, 2, 2, 1)
-setIndentation('nix', 2, 2, 2, 1)
-setIndentation('html', 2, 2, 2, 1)
+setIndentation("dart", 2, 2, 2, 1)
+setIndentation("c", 2, 2, 2, 1)
+setIndentation("nix", 2, 2, 2, 1)
+setIndentation("html", 2, 2, 2, 1)
