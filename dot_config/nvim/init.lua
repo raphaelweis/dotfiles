89cc9dbaf15b5aa39102aab1863efddae7c67798
Raphaël Weis <raphael.weis.g@gmail.com>
2025-02-26 14:58:42 +0100

    feat: i3 qol updates, alacritty config + remove unused stuff.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -203,6 +203,24 @@ require("lazy").setup({
 			opts = {},
 		},
 		{
+			"christoomey/vim-tmux-navigator",
+			cmd = {
+				"TmuxNavigateLeft",
+				"TmuxNavigateDown",
+				"TmuxNavigateUp",
+				"TmuxNavigateRight",
+				"TmuxNavigatePrevious",
+				"TmuxNavigatorProcessList",
+			},
+			keys = {
+				{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+				{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+				{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+				{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+				{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+			},
+		},
+		{
 			"neovim/nvim-lspconfig",
 			dependencies = {
 				"hrsh7th/cmp-nvim-lsp",
