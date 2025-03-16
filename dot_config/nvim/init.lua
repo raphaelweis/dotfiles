32704ef715c4e48afb34efb399ab6e698bf92bef
Raphaël Weis <raphael.weis.g@gmail.com>
2025-03-16 21:55:53 +0100

    feat: Minor updates for ubuntu setup.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -382,6 +382,7 @@ require("lazy").setup({
 			priority = 1000,
 			config = function()
 				vim.g.gruvbox_material_foreground = "original"
+        vim.g.gruvbox_material_transparent_background = 1
 				vim.cmd.colorscheme("gruvbox-material")
 			end,
 		},
